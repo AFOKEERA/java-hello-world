55fdb8fc13a5e85d1c06e469617667ed8cae2507
AFOKEERA <fokeeraahsan58@gmail.com>
2026-01-11 22:55:53 +0400

    Add second hello message

--- src/com/example/demo/Main.java
+++ src/com/example/demo/Main.java
@@ -3,6 +3,6 @@ package com.example.demo;
 public class Main {
 	public static void main(String[] args) {
 		//Hello World!!!!!!!!!!!!!!
-        System.out.println("Hello World");
+        System.out.println("Hello Java World");
     }
 }
